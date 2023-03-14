fd958925dd89e00fd5989531b4523f62e6752bb1
JaredAzevedoSSM <jaredssm@stanford.edu>
2023-03-14 12:33:56 -0700

    Update models.py

--- models.py
+++ models.py
@@ -213,22 +213,20 @@ def compute(inputpath, lmodel, debug, b=16, e=1):
     euc_predictions = custom_similarity(embeddings, amendment_embeddings, "euclidean")
     man_predictions = custom_similarity(embeddings, amendment_embeddings, "manhattan")
     min_predictions = custom_similarity(embeddings, amendment_embeddings, "minkowski")
-    sem_predictions = util.semantic_search(embeddings, amendment_embeddings)
-    print(sem_predictions)
+    #sem_predictions = util.semantic_search(embeddings, amendment_embeddings)
     
     for sample in range(len(embeddings)):
         cos_final_predictions.append(np.argmax(cos_predictions[sample]))
         euc_final_predictions.append(np.argmin(euc_predictions[sample]))
         man_final_predictions.append(np.argmin(man_predictions[sample]))
         min_final_predictions.append(np.argmin(min_predictions[sample]))
-        print(sem_predictions[sample])
-        sem_final_predictions.append(np.argmax(sem_predictions[sample]))
+        #sem_final_predictions.append(np.argmax(sem_predictions[sample]))
 
     evaluate(test_data, cos_final_predictions, "cosine")
     evaluate(test_data, euc_final_predictions, "euclidean distance")
     evaluate(test_data, man_final_predictions, "manhattan distance")
     evaluate(test_data, min_final_predictions, "minkowski distance")
-    evaluate(test_data, sem_final_predictions, "semantic search")
+    #evaluate(test_data, sem_final_predictions, "semantic search")
 
     print("\nThe program has finished running.\n")
 
